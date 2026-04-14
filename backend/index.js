7b2f7698e0491c6d19848e65eae28c7e2441f527
Mohd Intakhab Alam <intakhab087@gmail.com>
2026-04-14 23:10:42 +0530

    add: connect mongodb atlas database

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,20 @@
 const express = require("express");
 const cors = require("cors");
+const dotenv = require("dotenv");
+const connectDB = require("./db");
+
+//load env variables
+dotenv.config();
 
 const app = express();
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 //middleware
 app.use(cors());
 app.use(express.json()); //to parse json body
+
+//connect to database
+connectDB();
 
 //test route
 app.get("/", (req, res) => {
